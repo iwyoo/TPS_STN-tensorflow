89469b90aa7dc2093e474b94810381f43b931a4f
iwyoo <doveu1234@gmail.com>
2016-11-03 22:09:57 +0900

    minor change

--- test.py
+++ test.py
@@ -4,24 +4,23 @@ from PIL import Image
 from TPS_STN import TPS_STN
 
 img = np.array(Image.open("original.png"))
-shape = list(img.shape)
-out_size = shape
+out_size = list(img.shape)
+shape = [1]+out_size+[1]
 
 nx = 2
 ny = 2
-iteration = 10000
 
 v = np.array([
-  [0.1, 0.1],
   [0.2, 0.2],
-  [0.2, 0.2],
-  [0.4, 0.4]])
+  [0.4, 0.4],
+  [0.6, 0.6],
+  [0.8, 0.8]])
 
 p = tf.constant(v.reshape([1, nx*ny, 2]), dtype=tf.float32)
-t_img = tf.constant(img.reshape([1,shape[0],shape[1],1]), dtype=tf.float32)
+t_img = tf.constant(img.reshape(shape), dtype=tf.float32)
 t_img = TPS_STN(t_img, nx, ny, p, out_size)
 
 with tf.Session() as sess:
   sess.run(tf.initialize_all_variables())
   img1 = sess.run(t_img)
-  Image.fromarray(np.uint8(img1.reshape(shape))).save("transformed.png") 
+  Image.fromarray(np.uint8(img1.reshape(out_size))).save("transformed.png") 
